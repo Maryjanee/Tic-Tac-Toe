bcfce3f09150ed4f30509fb4b94b606eb8ca3fd0
Ahmad Chata <ahmadchata@gmail.com>
2020-07-28 11:16:03 +0100

    Add Board class and display_board method

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-puts 'Hello World'
+class Board
+    attr_accessor:board
+
+    @board = Array.new(9, " ")
+    def initialize
+        @board = board
+    end
+    
+    def display_board
+        puts "-------------"
+        puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+        puts "-------------"
+        puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+        puts "-------------"
+        puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+        puts "-------------"   
+    end
+end
+board1 = Board.new(board)
+puts board1
