d57c664c2e6b59e44bb90d0b455e2a6a300d2d5b
Ahmad Chata <ahmadchata@gmail.com>
2020-07-28 15:17:40 +0100

    Add player_selection method

--- bin/main.rb
+++ bin/main.rb
@@ -1,48 +1,58 @@
 #!/usr/bin/env ruby
-=begin
-Game Pseudocode
-1.Create board
-2.Get user input from players
-3.Track players and turns
-4.Check if board is full
-5.check for valid move
-6.check positions for each player
-7.check for winner, loser and draw
-8. display output
-
-=end
+# Game Pseudocode
+# 1.Create board
+# 2.Get user input from players
+# 3.Track players and turns
+# 4.Check if board is full
+# 5.check for valid move
+# 6.check positions for each player
+# 7.check for winner, loser and draw
+# 8. display output
+#
 
 class Board
-    attr_reader:board
-    
-    def initialize
-      reset_board
+  attr_reader :board
+
+  def initialize
+    reset_board
+  end
+
+  def reset_board
+    @board = Array.new(9, ' ')
+  end
+
+  def display_board
+    puts '-------------'
+    puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+    puts '-------------'
+    puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+    puts '-------------'
+    puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+    puts '-------------'
+  end
+
+  def user_input
+    puts "Enter first player's Name"
+    player_one = gets.chomp
+    puts "Enter second player's Name"
+    player_two = gets.chomp
+    puts "#{player_one} you are X \n#{player_two} you are O"
+  end
+
+  def player_selection
+    puts 'The Game has begun, make your selection'
+    while (selection = gets.chomp.to_i)
+      if selection.between?(0, 9) != true
+        puts 'Please choose a number between 1 - 9'
+      else
+        puts 'weldone'
+        break
+      end
     end
-    
-    def reset_board
-      @board = Array.new(9, " ")
-    
-    end
-    
-    def display_board
-        puts "-------------"
-        puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
-        puts "-------------"
-        puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
-        puts "-------------"
-        puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
-        puts "-------------"   
-    end
-    
-    def get_user_input
-      puts "Enter first player's Name"
-      player_one = gets.chomp
-      puts "Enter second player's Name"
-      player_two = gets.chomp
-      puts "#{player_one} you are X \n#{player_two} you are O"
-    end
+  end
 end
 
-board1 = Board.new()
+board1 = Board.new
 puts board1.display_board
-puts board1.get_user_input
+puts board1.user_input
+puts board1.player_selection
