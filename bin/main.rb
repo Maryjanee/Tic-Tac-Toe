53f21cdf09d82d996ff108acbcbe7b8735091604
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-07-29 06:07:54 +0100

    Add player_selection method

--- bin/main.rb
+++ bin/main.rb
@@ -31,22 +31,33 @@ class Board
     puts '-------------'
   end
 
-  def user_input
+  def start_game
     puts "Enter first player's Name"
     player_one = gets.chomp
     puts "Enter second player's Name"
     player_two = gets.chomp
     puts "#{player_one} you are X \n#{player_two} you are O"
+    @player_count = 0
   end
 
-  def player_selection
-    puts 'The Game has begun, make your selection'
-    while (selection = gets.chomp.to_i)
-      if selection.between?(0, 9) != true
+  def player_selection(player)
+    puts "It's the #{player}'s turn, Please Choose a number between 1 - 9 "
+    input = gets.chomp
+    selection = input.to_i
+    if input != selection.to_s 
+      puts "this is not a number" 
+      player_selection
+    end
+    
+    sentinel = true
+    while sentinel
+      if selection.is_a?(Integer) == false || selection.between?(1, 9) == false 
         puts 'Please choose a number between 1 - 9'
-      else
-        puts 'weldone'
-        break
+        selection = gets.to_i
+       elsif selection.between?(1, 9) == true 
+        sentinel = false
+        puts "Your selection has been placed on the board"
+        @player_count += 1
       end
     end
   end
