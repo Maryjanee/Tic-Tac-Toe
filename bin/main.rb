3fc80363a244c6d77c084a7a4a9f0603e316eca8
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-07-30 10:56:33 +0100

    update player_selection loop

--- bin/main.rb
+++ bin/main.rb
@@ -49,30 +49,20 @@ class Player
     puts "Enter second player's Name"
     @@player_two = gets.chomp
     puts "#{@@player_one} you are #{PLAYER_ONE_TAG} \n#{@@player_two} you are #{PLAYER_TWO_TAG}"
-  
   end
   
-  def player_selection(player)
-    puts "It's the #{player}'s turn, Please Choose a number between 1 - 9 "
-    input = gets.chomp
-    selection = input.to_i
-    if input != selection.to_s
-      puts 'this is not a number'
-      player_selection
-    end
-    sentinel = true
-    while sentinel
-      if selection.is_a?(Integer) == false || selection.between?(1, 9) == false
-        puts 'Please choose a number between 1 - 9'
-        selection = gets.to_i
-      elsif selection.between?(1, 9) == true
-        sentinel = false
-        puts 'Your selection has been placed on the board'
-        @player_count += 1
-      end
+  def player_selection
+    puts "It's the #{current_player}'s turn, Please Choose a number between 1 - 9 "
+    input = gets.chomp.to_i
+      
+    while input.between?(1, 9) == false
+      puts 'Invalid! Please choose a number between 1 - 9'
+       input = gets.chomp.to_i
     end
   end
 end
+  
+ 
 
 board1 = Board.new
 puts board1.display_board
