85d465d8a359c53f48108937ab11100efb371645
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-07-31 08:25:37 +0100

    Update player_selection method to dislay winner;s name

--- bin/main.rb
+++ bin/main.rb
@@ -36,6 +36,8 @@ end
 class Player
   PLAYER_ONE_TAG = 'X'
   PLAYER_TWO_TAG = 'O'
+  @@player_one
+  @@player_one
   
   @@play_counter = 0
 
@@ -86,7 +88,13 @@ class Player
 
     check = (@@gameBoard.board.values_at(0, 1, 2).all?("X") || @@gameBoard.board.values_at(0, 1, 2).all?("O") || @@gameBoard.board.values_at(3, 4, 5).all?("X") || @@gameBoard.board.values_at(3, 4, 5).all?("O") || @@gameBoard.board.values_at(6, 7, 8).all?("X") || @@gameBoard.board.values_at(6, 7, 8).all?("O") || @@gameBoard.board.values_at(0, 3, 6).all?("X") || @@gameBoard.board.values_at(0, 3, 6).all?("O") || @@gameBoard.board.values_at(1, 4, 7).all?("X") || @@gameBoard.board.values_at(1, 4, 7).all?("O") || @@gameBoard.board.values_at(2, 5, 8).all?("X") || @@gameBoard.board.values_at(2, 5, 8).all?("O") || @@gameBoard.board.values_at(0, 4, 8).all?("X") || @@gameBoard.board.values_at(0, 4, 8).all?("O") || @@gameBoard.board.values_at(2, 4, 8).all?("X") || @@gameBoard.board.values_at(2, 4, 8).all?("O"))
    if check
-    puts "We have a winner"
+     x_count = @@gameBoard.board.count{|x| x == "X"}
+     o_count = @@gameBoard.board.count{|x| x == "O"}
+     if(x_count > o_count) 
+      puts "#{@@player_one} is the winner"
+     else 
+      puts "#{@@player_two} is the winner"
+     end
     elsif ( !check && @@gameBoard.board.any? { |e| e.is_a?(Integer)} )
       player_selection
     else
