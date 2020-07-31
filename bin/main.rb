dbb95295d5a84cdc1d99bf0ad4c1b1a49fe7e45c
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-07-31 11:43:32 +0100

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
-
+# rubocop:disable Layout/LineLength
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 require './lib/Player.rb'
 require './lib/Board.rb'
 
@@ -13,57 +17,59 @@ def display_board(board)
   puts '-------------'
 end
 
-def player_selection(game_board, player)
+def player_selection(gameboard, player)
   puts "It's the #{player.current_player}'s turn, Please Choose a number between 1 - 9 "
   input = gets.chomp.to_i
-    
+
   while input.between?(1, 9) == false
     puts 'Invalid! Please choose a number between 1 - 9'
-     input = gets.chomp.to_i
+    input = gets.chomp.to_i
   end
-  
-  if player.is_valid_move(input)
+
+  if player.valid_move(input)
     player.insert_into_board(input)
   else
-    puts "Invalid move, try again."
-    player_selection(game_board, player)
+    puts 'Invalid move, try again.'
+    player_selection(gameboard, player)
   end
 
-  display_board(game_board.board)
+  display_board(gameboard.board)
 
-  check = (game_board.board.values_at(0, 1, 2).all?("X") || game_board.board.values_at(0, 1, 2).all?("O") || game_board.board.values_at(3, 4, 5).all?("X") || game_board.board.values_at(3, 4, 5).all?("O") || game_board.board.values_at(6, 7, 8).all?("X") || game_board.board.values_at(6, 7, 8).all?("O") || game_board.board.values_at(0, 3, 6).all?("X") || game_board.board.values_at(0, 3, 6).all?("O") || game_board.board.values_at(1, 4, 7).all?("X") || game_board.board.values_at(1, 4, 7).all?("O") || game_board.board.values_at(2, 5, 8).all?("X") || game_board.board.values_at(2, 5, 8).all?("O") || game_board.board.values_at(0, 4, 8).all?("X") || game_board.board.values_at(0, 4, 8).all?("O") || game_board.board.values_at(2, 4, 8).all?("X") || game_board.board.values_at(2, 4, 8).all?("O"))
+  check = (gameboard.board.values_at(0, 1, 2).all?('X') || gameboard.board.values_at(0, 1, 2).all?('O') || gameboard.board.values_at(3, 4, 5).all?('X') || gameboard.board.values_at(3, 4, 5).all?('O') || gameboard.board.values_at(6, 7, 8).all?('X') || gameboard.board.values_at(6, 7, 8).all?('O') || gameboard.board.values_at(0, 3, 6).all?('X') || gameboard.board.values_at(0, 3, 6).all?('O') || gameboard.board.values_at(1, 4, 7).all?('X') || gameboard.board.values_at(1, 4, 7).all?('O') || gameboard.board.values_at(2, 5, 8).all?('X') || gameboard.board.values_at(2, 5, 8).all?('O') || gameboard.board.values_at(0, 4, 8).all?('X') || gameboard.board.values_at(0, 4, 8).all?('O') || gameboard.board.values_at(2, 4, 8).all?('X') || gameboard.board.values_at(2, 4, 8).all?('O'))
   if check
-    x_count = game_board.board.count{|x| x == "X"}
-    o_count = game_board.board.count{|x| x == "O"}
-    if(x_count > o_count) 
+    x_count = gameboard.board.count { |x| x == 'X' }
+    o_count = gameboard.board.count { |x| x == 'O' }
+    if x_count > o_count
       puts "#{player.player_one} is the winner"
-    else 
+    else
       puts "#{player.player_two} is the winner"
     end
-    elsif ( !check && game_board.board.any? { |e| e.is_a?(Integer)} )
-      player_selection(game_board, player)
-    else
-      puts "Its a draw"
-    end
+  elsif !check && gameboard.board.any? { |e| e.is_a?(Integer) }
+    player_selection(gameboard, player)
+  else
+    puts 'Its a draw'
   end
+end
 
 def start_game
-  game_board = Board.new
-  display_board(game_board.board)
+  gameboard = Board.new
+  display_board(gameboard.board)
 
   puts "Enter first player's Name"
   player_one = gets.chomp
   puts "Enter second player's Name"
   player_two = gets.chomp
 
-  player = Player.new(game_board, player_one, player_two)
+  player = Player.new(gameboard, player_one, player_two)
 
   puts "#{player_one} you are #{player.player_one_tag} \n#{player_two} you are #{player.player_two_tag}"
 
-  player_selection(game_board, player)
+  player_selection(gameboard, player)
 end
 
 start_game
-
-
-
+# rubocop:enable Layout/LineLength
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/AbcSize
