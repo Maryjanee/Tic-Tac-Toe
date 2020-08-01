60d1997e25a6b6bf15fd1c504730fda0b853647c
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-01 03:18:46 +0100

    update player class with winner checker method

--- lib/player.rb
+++ lib/player.rb
@@ -24,6 +24,11 @@ class Player
     @gameboard.board[input - 1] = @play_counter.even? ? @player_one_tag : @player_two_tag
     @play_counter += 1
   end
+ 
+  def winner_checker(gameboard)
+    check = (gameboard.values_at(0, 1, 2).all?('X') || gameboard.values_at(0, 1, 2).all?('O') || gameboard.values_at(3, 4, 5).all?('X') || gameboard.values_at(3, 4, 5).all?('O') || gameboard.values_at(6, 7, 8).all?('X') || gameboard.values_at(6, 7, 8).all?('O') || gameboard.values_at(0, 3, 6).all?('X') || gameboard.values_at(0, 3, 6).all?('O') || gameboard.values_at(1, 4, 7).all?('X') || gameboard.values_at(1, 4, 7).all?('O') || gameboard.values_at(2, 5, 8).all?('X') || gameboard.values_at(2, 5, 8).all?('O') || gameboard.values_at(0, 4, 8).all?('X') || gameboard.values_at(0, 4, 8).all?('O') || gameboard.values_at(2, 4, 8).all?('X') || gameboard.values_at(2, 4, 8).all?('O'))
+    check
+  end
 end
 
 # rubocop:enable Layout/LineLength
