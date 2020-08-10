f44b80f1bce238254f4b17fdc8e5467c5e31d15e
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-10 20:16:48 +0100

    add test for negative cases

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -8,14 +8,14 @@ describe 'Player' do
   let(:input) { 5 }
 
   describe '#winner_checker' do
-    it 'return the true if a win is found' do
+    it 'return true if a win is found' do
       player = Player.new(gameboard, player_one, player_two)
       expect(player.winner_checker(gameboard)).to eql(true)
     end
-
+    
     it 'returns false when the board is full ' do
       player = Player.new(['✘', '◯', '✘', '◯', '✘', '✘', '◯', '✘', '◯'], player_one, player_two)
-      expect(player.winner_checker(['✘', '◯', '✘', '◯', '✘', '✘', '◯', '✘', '◯'])).to eql(false)
+      expect(player.winner_checker(['✘', '◯', '✘', '◯', '✘', '✘', '◯', '✘', '◯'])).to be(false)
     end
     it 'returns false when the board is not full and no win is found ' do
       player = Player.new(['✘', '◯', '✘', '◯', 5, '✘', '◯', '✘', '◯'], player_one, player_two)
@@ -27,6 +27,11 @@ describe 'Player' do
     it 'returns true for an valid move ' do
       player = Player.new(['✘', '✘', 2, 3, '◯', '◯', 7, 8, 9], player_one, player_two)
       expect(player.valid_move(8)).to eql(true)
+    end
+    
+    it 'returns false when the board is not full and an invalid move is chosen ' do
+      player = Player.new(['✘','◯', 3, '✘', 5, 6, 7, 8, 9], player_one, player_two)
+      expect(player.valid_move(9)).to be(true)
     end
 
     it 'returns true for a valid range of valid input ' do
